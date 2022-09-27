dc64bf0944f64b21f0cea48142761c6498c9c4b9
Esha Verma <eshaverma8302@gmail.com>
2022-09-27 17:03:53 +0530

    Frontend Completed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,12 @@
-import './App.css';
-import { Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import BlogList from './Pages/BlogList';
 import BlogDetail from './Pages/BlogDetail';
-import Home from './Pages/Home';
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/blogs" element={<BlogList/>}/>
+        <Route path="/" element={<BlogList/>}/>
         <Route path="blog-detail/:id" element={<BlogDetail/>}/>
       </Routes>
     </>
